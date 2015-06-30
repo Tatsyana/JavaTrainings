09eff11cdf51a828ccf30108f8fc4d7e226676a1
Tatsyana <justicia1990@mail.ru>
2015-07-01 00:36:53 +0300

    task01: Создала 2 метода: validateNumberPhone, validateName с использованием регулярных выражений. Также теперь при вводе фамилии, имени или номера телефона в неправильном формате программа не завершает действие, а повторно запрашивает корректные данные (циклит до ввода в нужном формате).

--- Homeworks/src/com/itclass/adressbook/utils/Helper.java
+++ Homeworks/src/com/itclass/adressbook/utils/Helper.java
@@ -5,10 +5,13 @@ import com.itclass.adressbook.domain.NumberPhone;
 import com.itclass.adressbook.domain.Record;
 import com.itclass.adressbook.repository.InMemoryRepository;
 
+import javax.xml.bind.ValidationException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Tatsyana.
@@ -17,56 +20,94 @@ public class Helper {
 
     public static void addRecord() {
         Record record = null;
-        try {
-            record = readPhone();
-        } catch (Exception e) {
-            System.err.println("Ошибка ввода: " + e);
+            try {
+                record = readPhone();
+            } catch (Exception e) {
+                System.err.println("Ошибка ввода: " + e);
         }
+
         InMemoryRepository.getInstance().add(record);
     }
 
     public static Record readPhone() throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Добавить имя: ");
-        String name = br.readLine();
-
-        System.out.println("Добавьте фамилию: ");
-        String fam = br.readLine();
-
-        System.out.println("Добавить номер телефона: ");
-        String number = br.readLine();
-        validateNumberPhone(number);
-
-        System.out.println("Тип номера: ");
-        System.out.println("1-Домашний: ");
-        System.out.println("2-Рабочий: ");
-        System.out.println("3-Мобильный: ");
-        String type = br.readLine();
-        switch (type) {
-            case "1":
-                type = "Домашний";
-                break;
-            case "2":
-                type = "Рабочий";
-                break;
-            case "3":
-                type = "Мобильный";
-                break;
-            default:
-                type = "Не определен";
-                break;
+        String name = null;
+        while (true) {
+            System.out.println("Добавить имя: ");
+            name = br.readLine();
+            try {
+                validateName(name);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
         }
-        NumberPhone numberPhone = new NumberPhone(number, type);
-        System.out.println("Категория: ");
-        System.out.println("Нажмите 1 - Друзья: ");
-        System.out.println("Нажмите 2 - Семья: ");
-        System.out.println("Нажмите 3 - Коллеги: ");
-        String key = br.readLine();
-        Category cat = Category.getCategory(key);
 
-        return Record.create(name, fam, numberPhone, cat);
-    }
+        String fam;
+        while (true) {
+        System.out.println("Добавьте фамилию: ");
+         fam = br.readLine();
+            try {
+                validateName(fam);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
+        }
+        String number;
+        while(true) {
+        System.out.println("Добавить номер телефона (не менее 6-ти символов): ");
+        number = br.readLine();
+
+            try {
+                validateNumberPhone(number);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
+        }
+
+
+            System.out.println("Тип номера: ");
+            System.out.println("1-Домашний: ");
+            System.out.println("2-Рабочий: ");
+            System.out.println("3-Мобильный: ");
+            String type = br.readLine();
+            switch (type) {
+                case "1":
+                    type = "Домашний";
+                    break;
+                case "2":
+                    type = "Рабочий";
+                    break;
+                case "3":
+                    type = "Мобильный";
+                    break;
+                default:
+                    type = "Не определен";
+                    break;
+            }
+
+            NumberPhone numberPhone = new NumberPhone(number, type);
+            System.out.println("Категория: ");
+            System.out.println("Нажмите 1 - Друзья: ");
+            System.out.println("Нажмите 2 - Семья: ");
+            System.out.println("Нажмите 3 - Коллеги: ");
+            String key = br.readLine();
+            Category cat = Category.getCategory(key);
+
+            return Record.create(name, fam, numberPhone, cat);
+        }
 
     public static long readId(){
 
@@ -99,50 +140,70 @@ public class Helper {
         System.out.println("Измененное имя: ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String firstName = null;
-        try {
-            firstName = br.readLine();
-        } catch (IOException e) {
-            System.err.println("Ошибка ввода." + e);//ввод разделен от изменения
+        while (true) {
+            try {
+                firstName = br.readLine();
+                validateName(firstName);
+            } catch (IOException e) {
+                System.err.println("Ошибка ввода." + e);//ввод разделен от изменения
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
         }
         record.setFirstName(firstName);
 
         System.out.println("Измененная фамилия: ");
         String lastName = null;
-        try {
-            lastName = br.readLine();
-        } catch (IOException e) {
-            System.err.println("Ошибка ввода." + e);
+        while (true) {
+            try {
+                lastName = br.readLine();
+                validateName(lastName);
+            } catch (IOException e) {
+                System.err.println("Ошибка ввода." + e);
+            }catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+            break;
         }
         record.setLastName(lastName);
 
         System.out.println("Измененный номер телефона: ");
         String number = null;
         String type = null;
-        try {
-            number = br.readLine();
-            validateNumberPhone(number);
+        while (true) {
+            try {
+                number = br.readLine();
+                validateNumberPhone(number);
 
-            System.out.println("Измененный тип телефона: ");
-            System.out.println("1-Домашний: ");
-            System.out.println("2-Рабочий: ");
-            System.out.println("3-Мобильный: ");
-            type = br.readLine();
-            switch (type) {
-                case "1":
-                    type = "Домашний";
-                    break;
-                case "2":
-                    type = "Рабочий";
-                    break;
-                case "3":
-                    type = "Мобильный";
-                    break;
-                default:
-                    type = "Не определен";
-                    break;
+                System.out.println("Измененный тип телефона: ");
+                System.out.println("1-Домашний: ");
+                System.out.println("2-Рабочий: ");
+                System.out.println("3-Мобильный: ");
+                type = br.readLine();
+                switch (type) {
+                    case "1":
+                        type = "Домашний";
+                        break;
+                    case "2":
+                        type = "Рабочий";
+                        break;
+                    case "3":
+                        type = "Мобильный";
+                        break;
+                    default:
+                        type = "Не определен";
+                        break;
+                }
+            } catch (IOException e) {
+                System.err.println("Ошибка ввода." + e);
+            } catch (ValidationException e) {
+                System.out.println(e.getMessage());
+                continue;
             }
-        } catch (IOException e) {
-            System.err.println("Ошибка ввода." + e);
+            break;
         }
         record.setPhone(new NumberPhone(number, type));
 
@@ -200,18 +261,39 @@ public class Helper {
         }
     }
 
-    public static int validateNumberPhone(String number) {
-        try {
-            return Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            System.err.println("Неверный формат" + e);
-            throw new NumberFormatException("Неверный формат: " + e);
-        }
+//    public static int validateNumberPhone(String number) {
+//        try {
+//            return Integer.parseInt(number);
+//        } catch (NumberFormatException e) {
+//            System.err.println("Неверный формат" + e);
+//            throw new NumberFormatException("Неверный формат: " + e);
+//        }
+//    }
+
+    public static void validateNumberPhone(String number) throws IOException, ValidationException {
+        final String PHONE_NUMBER_PATTERN =
+                "^(\\s*\\d\\s*){6,12}$";
+        Pattern pattern = Pattern.compile(PHONE_NUMBER_PATTERN, Pattern.CASE_INSENSITIVE);
+        String inputText = number;
+            Matcher matcher = pattern.matcher(inputText);
+            if (!matcher.matches()) {
+                throw new ValidationException("Неверный формат: " );
+            }
+
     }
 
 
+    public static void validateName(String name) throws IOException, ValidationException {
+        final String NAME_PATTERN =
+                "^[a-zA-Z|\\s]{1,100}$";
+        Pattern pattern = Pattern.compile(NAME_PATTERN, Pattern.CASE_INSENSITIVE);
+        String inputText = name;
+        Matcher matcher = pattern.matcher(inputText);
+        if (!matcher.matches()) {
+            throw new ValidationException("Неверный формат: " );
+        }
 
-
+    }
 
 
 
