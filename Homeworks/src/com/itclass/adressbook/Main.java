7a475b0a680ddb62ebe8916838f3e5d8b20c33c3
Tatsyana <justicia1990@mail.ru>
2015-07-25 22:09:21 +0300

    добавлен доступ по паролю

--- Homeworks/src/com/itclass/adressbook/Main.java
+++ Homeworks/src/com/itclass/adressbook/Main.java
@@ -3,6 +3,7 @@ package com.itclass.adressbook;
 import com.itclass.adressbook.domain.Category;
 import com.itclass.adressbook.domain.NumberPhone;
 import com.itclass.adressbook.domain.Record;
+import com.itclass.adressbook.domain.User;
 import com.itclass.adressbook.repository.InMemoryRepository;
 import com.itclass.adressbook.utils.Helper;
 
@@ -15,67 +16,121 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
 
         InMemoryRepository.getInstance().add(Record.create("Александр", "Сайчук", new NumberPhone("Мобильный", "37545"), Category.NONE));
         InMemoryRepository.getInstance().add(Record.create("Людмила", "Афанасенко", new NumberPhone("Домашний", "12345"), Category.COLLEAGUE));
         InMemoryRepository.getInstance().add(Record.create("Татьяна", "Шерстобитова", new NumberPhone("Рабочий", "2345"), Category.FAMILY));
-    //    Repository<Record, Long> repository = new BinaryRepository("Homeworks/src/binary.txt");
+        //    Repository<Record, Long> repository = new BinaryRepository("Homeworks/src/binary.txt");
         Helper.init(InMemoryRepository.getInstance());
         String key = "";
-        while (true) {
-            System.out.println("Press add or a to add: ");
-            System.out.println("Press del or d to delete: ");
-            System.out.println("Press edit or e to edit: ");
-            System.out.println("Press sort or s to sort ");
-            System.out.println("Press print to print: ");
-            System.out.println("Press filter or f to print: ");
-            System.out.println("Press 0 for exit");
+        User admin = new User("Tania", "13579", "Admin");
+        User user1 = new User("Kate", "67543", "User");
+        User user2 = new User("Igor", "000012", "User");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String login;
+        String password;
+        System.out.println("Введите логин: ");
+        login = br.readLine();
+        System.out.println("Введите пароль");
+        password = br.readLine();
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            try {
-                key = br.readLine();
-            } catch (IOException e) {
-                System.err.println("Ошибка ввода." + e);
-            }
-            key = key.toLowerCase();
-            switch (key) {
-                case "add":
-                case "a":
-                    Helper.getInstance().addRecord();
-                    break;
-                case "del":
-                case "d":
-                    Helper.getInstance().printAllRecord();
-                    Helper.getInstance().deleteRecord();
-                    break;
-                case "edit":
-                case "e":
-                    // предложить вывести всех
-                    Helper.getInstance().printAllRecord();
-                    Helper.getInstance().editRecord();
-                    break;
-                case "sort":
-                case "s":
-                    Helper.getInstance().sortRecord();
 
-                    //осознанно не поставлен break: после сортировки вывод информации
-                case "p":
-                case "print":
-                    Helper.getInstance().printAllRecord();
-                    //        printBooks(bookList);
-                    break;
-                case "f":
-                case "filter":
-                    Helper.getInstance().printAllRecord(Helper.getInstance().filter());
-                    break;
-                case "0":
-                    return;
-                default:
-                    System.out.println("Вы ввели неверное значение. Повторите попытку. ");
-                    break;
+        if (login.equals(admin.getLogin()) && password.equals(admin.getPassword())) {
+            while (true) {
+                System.out.println("Press add or a to add: ");
+                System.out.println("Press del or d to delete: ");
+                System.out.println("Press edit or e to edit: ");
+                System.out.println("Press sort or s to sort ");
+                System.out.println("Press print to print: ");
+                System.out.println("Press filter or f to print: ");
+                System.out.println("Press 0 for exit");
+
+                //  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                try {
+                    key = br.readLine();
+                } catch (IOException e) {
+                    System.err.println("Ошибка ввода." + e);
+                }
+                key = key.toLowerCase();
+                switch (key) {
+                    case "add":
+                    case "a":
+                        Helper.getInstance().addRecord();
+                        break;
+                    case "del":
+                    case "d":
+                        Helper.getInstance().printAllRecord();
+                        Helper.getInstance().deleteRecord();
+                        break;
+                    case "edit":
+                    case "e":
+                        // предложить вывести всех
+                        Helper.getInstance().printAllRecord();
+                        Helper.getInstance().editRecord();
+                        break;
+                    case "sort":
+                    case "s":
+                        Helper.getInstance().sortRecord();
+
+                        //осознанно не поставлен break: после сортировки вывод информации
+                    case "p":
+                    case "print":
+                        Helper.getInstance().printAllRecord();
+                        //        printBooks(bookList);
+                        break;
+                    case "f":
+                    case "filter":
+                        Helper.getInstance().printAllRecord(Helper.getInstance().filter());
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        System.out.println("Вы ввели неверное значение. Повторите попытку. ");
+                        break;
+                }
             }
+        } else if ((login.equals(user1.getLogin()) && password.equals(user1.getPassword())) || (login.equals(user2.getLogin()) && password.equals(user2.getPassword()))) {
+            while (true) {
+
+                System.out.println("Press sort or s to sort ");
+                System.out.println("Press print to print: ");
+                System.out.println("Press filter or f to print: ");
+                System.out.println("Press 0 for exit");
+
+                //   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                try {
+                    key = br.readLine();
+                } catch (IOException e) {
+                    System.err.println("Ошибка ввода." + e);
+                }
+                key = key.toLowerCase();
+                switch (key) {
+
+                    case "sort":
+                    case "s":
+                        Helper.getInstance().sortRecord();
+
+                        //осознанно не поставлен break: после сортировки вывод информации
+                    case "p":
+                    case "print":
+                        Helper.getInstance().printAllRecord();
+                        //        printBooks(bookList);
+                        break;
+                    case "f":
+                    case "filter":
+                        Helper.getInstance().printAllRecord(Helper.getInstance().filter());
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        System.out.println("Вы ввели неверное значение. Повторите попытку. ");
+                        break;
+                }
+            }
+        } else {
+
+            System.out.println("С таким логином и паролем у вас нет прав доступа");
         }
 
     }
---
+++ Homeworks/src/com/itclass/adressbook/domain/User.java
@@ -0,0 +1,42 @@
+package com.itclass.adressbook.domain;
+
+/**
+ * @author Tatsyana.
+ */
+public class User {
+    String login;
+    String password;
+    String role;
+
+    public User(String login, String password, String role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
